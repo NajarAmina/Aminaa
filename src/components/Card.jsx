efcb965e7e1ada2b2a494bc4fe5910ba67da0857
ramzy <amina220ami@gmail.com>
2025-07-12 05:54:15 +0100

    home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
-export function Card ({name ,price ,}) {
+export function Card ({name ,price,image,page}) {
     return <div className="row">
-                <img src="T_SHIRT.png" alt=" jacket"/>
+                <img src={image} alt={name} />
                 <div className="product-info-container">
                     <div className="product-info-left">
                         <h5>{name}</h5>
                         <p>{price}TND</p>
                         <div className="heart-icon">
-                            <i className='bx bx-heart' onclick="toggleFavorite(this, 'T-SHIRT', '69,90 TND', 'TSHIRT.png', 'T-SHIRT.html')" data-name="T-SHIRT"></i>
+                            <i className='bx bx-heart' onclick="toggleFavorite(name,price,image,page)" data-name={name} ></i>
                         </div>
                     </div>
                     <a href="#" className="products-btn add-to-cart">AJOUTER AU PANIER</a>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,8 @@
 import { Card } from "../Card";
-
+import './Hero.css';
  export function Hero(params) {
     return <><div className="home">
-        <div classNameName="home-text">
+        <div className="home-text">
             <h5>Nouvelle collection</h5>
             <h1>UN PAS D'AVANCE<br />SUR LA MODE</h1>
             <p>c'est ici que ca commence! </p>
@@ -14,47 +14,10 @@ import { Card } from "../Card";
             <h2>ON TREND</h2>
         </div>
         <div className="products">
-           <Card name="T-Shirt" price="90" />
-            <div className="row">
-                <img src="SAC A MAIN.png" alt=" jacket" />
-                <div className="product-info-container">
-                    <div className="product-info-left">
-                        <h5>SAC A MAIN</h5>
-                        <p>100,00 TND</p>
-                        <div className="heart-icon">
-                            <i className='bx bx-heart' onclick="toggleFavorite(this, 'SAC A MAIN', '100,00 TND', 'SAC A MAIN.png', 'sac.html')" data-name="SAC A MAIN"></i>
-                        </div>
-                    </div>
-                    <a href="#" className="products-btn add-to-cart">AJOUTER AU PANIER</a>
-                </div>
-            </div>
-           
-            <div className="row">
-                <img src="ROBE.png" alt=" jacket"/>
-                <div className="product-info-container">
-                    <div className="product-info-left">
-                        <h5>ROBE</h5>
-                        <p>180,00 TND</p>
-                        <div className="heart-icon">
-                            <i className='bx bx-heart' onclick="toggleFavorite(this, '>ROBE', '180,00 TND', 'ROBE.png', 'robe.html')" data-name="ROBE"></i>
-                        </div>
-                    </div>
-                    <a href="#" className="products-btn add-to-cart">AJOUTER AU PANIER</a>
-                </div>
-            </div>
-            <div className="row">
-                <img src="T_SHIRT.png" alt=" jacket"/>
-                <div className="product-info-container">
-                    <div className="product-info-left">
-                        <h5>T-SHIRT</h5>
-                        <p>69,90 TND</p>
-                        <div className="heart-icon">
-                            <i className='bx bx-heart' onclick="toggleFavorite(this, 'T-SHIRT', '69,90 TND', 'TSHIRT.png', 'T-SHIRT.html')" data-name="T-SHIRT"></i>
-                        </div>
-                    </div>
-                    <a href="#" className="products-btn add-to-cart">AJOUTER AU PANIER</a>
-                </div>
-            </div>
+            <Card name="JACKET"price="60.90" image="jacket.png" />
+            <Card name="SAC A MAIN" price="100.00"image="sac-a-main.png" />
+            <Card name="ROBE" price="180.00"image="robe.png" />
+            <Card name="T-Shirt"  price="69.00"image="t-shirt.png" />
         </div>
         <script src="js/vente.js"></script>
         <script src="js/panier.js"></script>
